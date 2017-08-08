a0bf7aabab7bb91cc83d9e416fad4c784d94d119
Fei Hu <fei.hu1@ibm.com@Feis-MacBook-Pro-2.local>
2017-08-08 15:35:55 -0700

    minor revision

--- src/main/scala/org/apache/sysml/examples/MNIST_Distrib_Sgd_Dummy.scala
+++ src/main/scala/org/apache/sysml/examples/MNIST_Distrib_Sgd_Dummy.scala
@@ -18,7 +18,6 @@ object MNIST_Distrib_Sgd_Dummy {
     conf.set("spark.testing.memory", memSize.toString)*/
 
     val sc = new SparkContext(conf)
-    sc.getConf.set()
     //sc.setLogLevel("TRACE")
 
     val ml = new MLContext(sc)
